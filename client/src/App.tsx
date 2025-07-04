07046ee4ce74279347e4996f76a890e8536bba9c
Willie Randolph <RazzleberryT@gmail.com>
2025-07-04 12:54:47 -0400

    Create App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>🌿 The Hippie Scientist</h1>
+      <p>Welcome to the psychedelic frontier.</p>
+    </div>
+  );
+}
+
+export default App;
