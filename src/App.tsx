ce0207819d3278f41071334cc1c1244df1e8edca
Willie Randolph <RazzleberryT@gmail.com>
2025-07-05 01:24:39 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-// src/App.tsx
 import React from 'react';
 import HerbIndex from './components/HerbIndex.tsx';
 
 function App() {
   return (
-    <div className="min-h-screen bg-black text-white font-sans">
+    <div className="min-h-screen bg-black text-white">
       <header className="text-center py-6">
         <h1 className="text-4xl font-bold text-teal-400">The Hippie Scientist</h1>
-        <p className="text-gray-300">Exploring Nature’s Psychedelics One Herb at a Time</p>
+        <p className="text-gray-300">Exploring Natural Psychoactive Herbs</p>
       </header>
       <main>
         <HerbIndex />
