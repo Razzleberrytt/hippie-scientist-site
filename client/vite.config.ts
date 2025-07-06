a85c4f0e88601325e2d17b7c4a4a2cf92108d614
Willie Randolph <RazzleberryT@gmail.com>
2025-07-05 20:39:02 -0400

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,15 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: '.', // Make sure this points to client root
   plugins: [react()],
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
   },
 });
