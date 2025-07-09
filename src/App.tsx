43b2a637156ab93f465a00e2ab4a643a72d7437f
Willie Randolph <RazzleberryT@gmail.com>
2025-07-09 13:55:13 -0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 
+console.log('✅ App is running');
+
 const App = () => {
   return (
     <div className="min-h-screen bg-white text-black text-center flex items-center justify-center text-3xl font-bold">
