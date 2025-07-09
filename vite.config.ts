39de311dd9209255f1fd9a2809d5c811decf099b
Willie Randolph <RazzleberryT@gmail.com>
2025-07-09 14:32:50 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hippie-scientist-site/', // ← match your GitHub repo name
+  base: '/hippie-scientist-site/', // must match GitHub repo name
 });
