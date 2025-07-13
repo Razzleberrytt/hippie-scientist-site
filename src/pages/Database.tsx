a31f59e7e967c32d8edd61e6ec6ddda0971e3ec1
Willie Randolph <RazzleberryT@gmail.com>
2025-07-12 22:03:23 -0400

    Update Database.tsx

--- src/pages/Database.tsx
+++ src/pages/Database.tsx
@@ -1,15 +1,15 @@
+// src/pages/Database.tsx
+
 import React from 'react';
-import HerbCard from '../components/HerbCard';
+import { HerbCard } from '../components/HerbCard';
 import { herbsData } from '../data/herbs';
 
-const DatabasePage: React.FC = () => {
+export default function Database() {
   return (
-    <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="database">
       {herbsData.map((herb) => (
         <HerbCard key={herb.id} herb={herb} />
       ))}
     </div>
   );
-};
-
-export default DatabasePage;
+}
