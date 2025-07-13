42f32cbb174a714d5dd602d4320de45945cb683a
Willie Randolph <RazzleberryT@gmail.com>
2025-07-12 21:42:26 -0400

    Add files via upload

--- src/data/herbs.ts
+++ src/data/herbs.ts
@@ -1,8 +1,4 @@
 import herbsDataRaw from './herbs.json';
 import type { Herb } from '../types/Herb';
 
-/**
- * Full herb database loaded from JSON.
- * Each object in herbsDataRaw conforms to the Herb interface.
- */
 export const herbsData: Herb[] = herbsDataRaw;
