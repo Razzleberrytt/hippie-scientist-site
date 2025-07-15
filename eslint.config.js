0072008f41b8eeb44a019cc1609889f26ad81ecd
Willie Randolph <RazzleberryT@gmail.com>
2025-07-15 14:51:33 -0400

    Remove built assets and restore dev index

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import prettier from "eslint-config-prettier";
 
 export default [
+  { ignores: ["dist/**", "node_modules/**"] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
