22b2dbe56836a8d5c0b9e2b91224ab5e1f2dc256
Razzleberrytt <Razzleberrytt@users.noreply.github.com>
2025-07-17 19:46:17 +0000

    deploy: a0a8edcc2fd4a2a750807222ce2d4f0676fc9ee8

--- assets/posts-41491eb0.js
+++
@@ -1,2 +0,0 @@
-const o=[{id:1,slug:"welcome-to-the-cosmos",title:"Welcome to the Cosmic Journal",excerpt:"An introduction to our explorations of plants and consciousness.",content:`# Welcome
-This is a sample post to demonstrate the new blog layout. Enjoy drifting through knowledge in our digital forest.`,date:"2024-05-01"}];export{o as p};
