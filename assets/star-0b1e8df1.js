15f72bcee85c0cee0d9685c75eb30c32773bac95
Razzleberrytt <Razzleberrytt@users.noreply.github.com>
2025-07-18 16:09:30 +0000

    deploy: 39162a66723bf2e8cc8f5980d3378d13dea5ddbb

--- assets/star-0b1e8df1.js
+++ assets/star-0b1e8df1.js
@@ -1,4 +1,4 @@
-import{l as o}from"./main-bcc93350.js";/**
+import{l as o}from"./main-abfc2e64.js";/**
  * @license lucide-react v0.298.0 - ISC
  *
  * This source code is licensed under the ISC license.
