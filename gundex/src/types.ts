6d615f1803394fb2340e5b6abbbebd83ec0d38b0
Willie Randolph <RazzleberryT@gmail.com>
2025-07-18 23:37:27 -0400

    Add Gundex React app

---
+++ gundex/src/types.ts
@@ -0,0 +1,12 @@
+export interface Gun {
+  name: string;
+  manufacturer: string;
+  type: string;
+  caliber: string;
+  action: string;
+  capacity: string;
+  weight: string;
+  countryOfOrigin: string;
+  tags: string[];
+  image?: string;
+}
