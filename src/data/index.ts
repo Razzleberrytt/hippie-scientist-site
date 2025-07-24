ca9132fd5f4971523b7c6ce275d55528e94fc8aa
Willie Randolph <RazzleberryT@gmail.com>
2025-07-24 13:27:49 -0400

    Reapply "Reapply "Reapply "docs: log reapply of herb page (#280)" (#281) (#282)" (#283) (#284)" (#285)
    This reverts commit 028bdd50da9ad0a7989e7ff8106e29953bf2de80.

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,3 +2,4 @@ export { default as herbsData } from "./herbs/herbs.json";
 export { herbs, compounds, masterHerbsAndCompounds } from './masterList';
 export { herbBlurbs } from "./herbs/blurbs";
 export { learnSections } from "./learnContent.enrichedXL";
+export { neuroHerbGraphData } from './neuroHerbGraphData'
