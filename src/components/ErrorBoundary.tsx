6dcfbbce4994c9f030754ba70248b2030b085487
Willie Randolph <RazzleberryT@gmail.com>
2025-07-26 22:38:05 -0400

    Add ErrorBoundary component and wrap database page (#326)

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,33 +1,38 @@
-import React from 'react';
+import React from 'react'
 
-interface State {
-  hasError: boolean;
+interface Props {
+  fallback?: React.ReactNode
 }
 
-export default class ErrorBoundary extends React.Component<React.PropsWithChildren<{}>, State> {
-  constructor(props: any) {
-    super(props);
-    this.state = { hasError: false };
+interface State {
+  hasError: boolean
+}
+export default class ErrorBoundary extends React.Component<React.PropsWithChildren<Props>, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = { hasError: false }
   }
 
   static getDerivedStateFromError(_: Error): State {
-    return { hasError: true };
+    return { hasError: true }
   }
 
   componentDidCatch(error: Error, info: React.ErrorInfo) {
-    console.error("Caught error in ErrorBoundary:", error, info);
+    console.error('Caught error in ErrorBoundary:', error, info)
   }
 
   render() {
     if (this.state.hasError) {
       return (
-        <div className="text-center text-red-600 border border-red-400 bg-red-100 p-4 rounded shadow">
-          <h2 className="text-xl font-bold">Something went wrong</h2>
-          <p className="mt-2 text-sm">Please refresh the page or try again later.</p>
-        </div>
-      );
+        this.props.fallback ?? (
+          <div className='rounded border border-red-400 bg-red-100 p-4 text-center text-red-600 shadow'>
+            <h2 className='text-xl font-bold'>Something went wrong</h2>
+            <p className='mt-2 text-sm'>Please refresh the page or try again later.</p>
+          </div>
+        )
+      )
     }
 
-    return this.props.children;
+    return this.props.children
   }
 }
