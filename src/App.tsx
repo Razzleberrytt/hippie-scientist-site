3a391b42e07c8e0807e5639cd5af8a062f17817b
Willie Randolph <RazzleberryT@gmail.com>
2025-07-26 22:22:37 -0400

    Add Navbar and blend route (#325)

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,24 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Database from './pages/Database';
 import Favorites from './pages/Favorites';
+import HerbBlender from './pages/HerbBlender';
 import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar';
 // Import other pages as needed
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/database" element={<Database />} />
-      <Route path="/favorites" element={<Favorites />} />
-      {/* Add other routes here */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/database" element={<Database />} />
+        <Route path="/blend" element={<HerbBlender />} />
+        <Route path="/favorites" element={<Favorites />} />
+        {/* Add other routes here */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
