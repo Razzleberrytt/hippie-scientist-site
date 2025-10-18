312fcb003ff6c362e1301e7e8e0d0776b1620fea
Willie Randolph <RazzleberryT@gmail.com>
2025-10-18 11:27:38 -0400

    feat: bootstrap LLM crypto bot (paper-only) (#595)

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,1 +1,9 @@
-# Husky shell shim
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
+#!/usr/bin/env sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
+
+They WILL FAIL in v10.0.0
+"
