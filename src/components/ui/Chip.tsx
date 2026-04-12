6d9e6778b3f787eeda9290a7430441e29434cf1c
Willie Randolph <RazzleberryT@gmail.com>
2026-04-12 17:57:57 -0400

    Polish dark UI typography and component consistency (#1247)

--- src/components/ui/Chip.tsx
+++ src/components/ui/Chip.tsx
@@ -1,7 +1,3 @@
 export default function Chip({ children }: { children: any }) {
-  return (
-    <span className="inline-block text-xs px-2 py-1 rounded-full bg-white/10 mr-2 mb-2">
-      {children}
-    </span>
-  );
+  return <span className='ds-pill mr-2 mb-2'>{children}</span>
 }
