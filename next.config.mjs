dd76729951db44794d1259e729d8f22a7a92f634
Willie Randolph <RazzleberryT@gmail.com>
2026-04-27 11:04:16 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
+  output: 'export',
   trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 }
 
 export default nextConfig
