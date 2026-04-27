3017100fce534f0eb35a1928f6703e4d01dfff51
Willie Randolph <RazzleberryT@gmail.com>
2026-04-27 11:09:23 -0400

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://thehippiescientist.net/sitemap.xml',
+    host: 'https://thehippiescientist.net',
+  }
+}
