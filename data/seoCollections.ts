57ee0290c8f6d5a353e6e148f82f58ba3aa7df4f
Willie Randolph <RazzleberryT@gmail.com>
2026-04-30 10:21:32 -0400

    Type empty SEO collections export

--- data/seoCollections.ts
+++ data/seoCollections.ts
@@ -1,1 +1,11 @@
-export const seoCollections = []
+export type SeoCollection = {
+  slug: string
+  title: string
+  description: string
+  itemType: 'herb' | 'compound'
+  filters: {
+    effectsAny?: string[]
+  }
+}
+
+export const seoCollections: SeoCollection[] = []
