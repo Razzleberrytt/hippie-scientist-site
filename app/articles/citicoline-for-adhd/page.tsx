a779c327b65d2e4a2717d9d2342e74987295d9db
Willie Randolph <RazzleberryT@gmail.com>
2026-06-22 11:32:53 -0400

    Add citicoline article route

---
+++ app/articles/citicoline-for-adhd/page.tsx
@@ -0,0 +1,9 @@
+import FocusAdhdArticlePage, { focusAdhdMetadata } from '@/components/articles/FocusAdhdArticlePage'
+
+const SLUG = 'citicoline-for-adhd'
+
+export const metadata = focusAdhdMetadata(SLUG)
+
+export default function Page() {
+  return <FocusAdhdArticlePage slug={SLUG} />
+}
