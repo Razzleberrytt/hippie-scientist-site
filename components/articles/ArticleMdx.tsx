44c3ed762fec0c78636b05e626f5c0ad90d586d4
Willie Randolph <RazzleberryT@gmail.com>
2026-06-26 13:40:59 -0400

    Demote MDX body H1 headings

--- components/articles/ArticleMdx.tsx
+++ components/articles/ArticleMdx.tsx
@@ -1,12 +1,17 @@
 import { MDXContent } from '@content-collections/mdx/react'
+import type { ComponentPropsWithoutRef } from 'react'
 import { useMDXComponents } from '@/mdx-components'
 
 type ArticleMdxProps = {
   code: string
 }
 
+function BodyHeadingOne({ children, ...props }: ComponentPropsWithoutRef<'h1'>) {
+  return <h2 {...props}>{children}</h2>
+}
+
 export default function ArticleMdx({ code }: ArticleMdxProps) {
-  const components = useMDXComponents({})
+  const components = useMDXComponents({ h1: BodyHeadingOne })
 
   return <MDXContent code={code} components={components} />
 }
