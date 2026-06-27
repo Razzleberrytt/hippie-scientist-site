47a766b0e8db83faa7a9e4a65846952d6ecda3cb
Willie Randolph <RazzleberryT@gmail.com>
2026-06-27 10:02:03 -0400

    Add static redirects for deprecated herb slugs

---
+++ app/herbs/ganoderma-lucidum/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function DeprecatedGanodermaLucidumPage() {
+  redirect('/herbs/reishi/')
+}
---
+++ app/herbs/phellodendron/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function DeprecatedPhellodendronPage() {
+  redirect('/herbs/phellodendron-amurense/')
+}
