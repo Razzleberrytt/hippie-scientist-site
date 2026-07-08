b22574bea55a9d2342d3c56fb236c44dc639a72c
Willie Randolph <RazzleberryT@gmail.com>
2026-07-08 14:15:25 -0400

    Add SEO appendix to omega-3 ADHD page

--- app/guides/adhd/omega-3-and-adhd/page.tsx
+++ app/guides/adhd/omega-3-and-adhd/page.tsx
@@ -1,10 +1,15 @@
 import FocusAdhdArticlePage, { focusAdhdMetadata } from '@/components/articles/FocusAdhdArticlePage'
+import { Omega3AdhdSeoAppendix } from '@/components/articles/AdhdSeoIdeaAppendix'
 
 const SLUG = 'omega-3-and-adhd'
 
 export const metadata = focusAdhdMetadata(SLUG)
 
 export default function Page() {
-  return <FocusAdhdArticlePage slug={SLUG} />
+  return (
+    <>
+      <FocusAdhdArticlePage slug={SLUG} />
+      <Omega3AdhdSeoAppendix />
+    </>
+  )
 }
-
