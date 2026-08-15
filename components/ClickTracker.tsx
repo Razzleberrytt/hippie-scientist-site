f3c5628934abaf3eec61ce0df6629428d8437277
Willie Randolph <RazzleberryT@gmail.com>
2026-08-15 18:45:21 -0400

    Backlog 881-885: mount feedback without regressing latest tracking

--- components/ClickTracker.tsx
+++ components/ClickTracker.tsx
@@ -11,6 +11,7 @@ import {
 import { CONSENT_CHANGE_EVENT, getConsent } from '@/lib/consent'
 import { loadAnalytics } from '../src/lib/loadAnalytics'
 import { trackRevenueEvent } from '../src/lib/revenue-tracking'
+import ProfileFeedbackControls from '@/components/feedback/ProfileFeedbackControls'
 
 function isAffiliateLink(link: HTMLAnchorElement) {
   const href = link.getAttribute('href') || ''
@@ -171,5 +172,5 @@ export default function ClickTracker() {
     }
   }, [])
 
-  return null
+  return <ProfileFeedbackControls />
 }
