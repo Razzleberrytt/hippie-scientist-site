3b785c4bdf19132496ea1539ce98c0779cb3039c
Willie Randolph <RazzleberryT@gmail.com>
2026-09-04 13:56:37 -0400

    refactor(analytics): route sleep CTA tracking through canonical analytics

--- components/SleepResearchNextActionLink.tsx
+++ components/SleepResearchNextActionLink.tsx
@@ -2,7 +2,7 @@
 
 import type { ReactNode } from 'react'
 import Link from 'next/link'
-import { trackSleepNextActionClick, type SleepNextAction } from '@/lib/sleep-next-actions-analytics'
+import { trackSleepNextActionClick, type SleepNextAction } from '@/lib/analytics'
 
 export default function SleepResearchNextActionLink({
   href,
