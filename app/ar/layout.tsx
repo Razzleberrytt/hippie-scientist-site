73359886faaf9855cd40fc76022aadf661c91d24
Willie Randolph <RazzleberryT@gmail.com>
2026-09-12 08:25:45 -0400

    fix: restore Arabic locale layout

---
+++ app/ar/layout.tsx
@@ -0,0 +1,5 @@
+import type { ReactNode } from 'react'
+
+export default function ArabicLayout({ children }: { children: ReactNode }) {
+  return <div lang='ar' dir='rtl' data-locale='ar'>{children}</div>
+}
